257cbf7f05e088c070ee0a60dfd096568b9accf3
Laurenz1606 <laurenz.rausche@gmail.com>
2021-08-04 14:05:26 +0200

    adder

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("tes2t")
+const or = (a, b) => a || b;
+const nor = (a, b) => !(a || b);
+const and = (a, b) => a && b;
+const nand = (a, b) => !(a && b);
+const xor = (a, b) => !!(!!a ^ !!b)
+const xnor = (a, b) => !(!!a ^ !!b)
+const not = (a) => !a;
+
+const adder = (a, b, ci = false) => {
+  const v1 = xor(a, b);
+  const v2 = and(v1, ci);
+  const v3 = and(a, b);
+  const sum = xor(v1, ci);
+  const co = or(v2, v3);
+  return [sum, co];
+};
